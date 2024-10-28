a9c12a28d6b55526098fb4c9ba691a7f500d20de
Ezequiel <ezequielgaribottovillanueva@gmail.com>
2024-10-28 16:29:30 +0100

    temperatura graficada i gota

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
+# Posicionamos la gota en el centro, inicialmente
+gota = False
+x=2
+y=2
 
+def on_forever():
+    if gota == False:
+        # Gràfic de temperatura
+        led.plot_bar_graph(input.temperature(), 50)
+
+    else:
+        # Moure gota a la pantalla
+        global x
+        global y
+        led.plot(x, y)
+        basic.pause(50)
+        led.unplot(x, y)
+        accX = input.acceleration(Dimension.X)
+        accY = input.acceleration(Dimension.Y)
+
+        if accX <= 150 and x > 0:
+            x=x-1
+        if accX > 150 and x < 4:
+            x=x+1
+        if accY <= 150 and x < 0:
+            y=y-1
+        if accY > 150 and y < 4:
+            y=y+1
+    pass
+basic.forever(on_forever)
+
+def on_button_pressed_b(): # Si l'usuari prem el botó B, s'activa el mode de gota (REPTE 7)
+    global gota
+    if gota == False:
+        gota = True
+        # Ocultem tots els leds si volem activar el mode de gota
+        for i in range(0,5):
+            for j in range(0,5):
+                led.unplot(i, j)
+    else: # Si l'usuari torna a prèmer el botó B, es torna a posar el mode de estació meteorologica  (REPTE 6)
+        gota = False
+input.on_button_pressed(Button.B, on_button_pressed_b)
--- main.ts
+++ main.ts
@@ -1,1 +1,56 @@
-
+//  Posicionamos la gota en el centro, inicialmente
+let gota = false
+let x = 2
+let y = 2
+basic.forever(function on_forever() {
+    let accX: number;
+    let accY: number;
+    if (gota == false) {
+        //  Gràfic de temperatura
+        led.plotBarGraph(input.temperature(), 50)
+    } else {
+        //  Moure gota a la pantalla
+        
+        
+        led.plot(x, y)
+        basic.pause(50)
+        led.unplot(x, y)
+        accX = input.acceleration(Dimension.X)
+        accY = input.acceleration(Dimension.Y)
+        if (accX <= 150 && x > 0) {
+            x = x - 1
+        }
+        
+        if (accX > 150 && x < 4) {
+            x = x + 1
+        }
+        
+        if (accY <= 150 && x < 0) {
+            y = y - 1
+        }
+        
+        if (accY > 150 && y < 4) {
+            y = y + 1
+        }
+        
+    }
+    
+    
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    //  Si l'usuari prem el botó B, s'activa el mode de gota (REPTE 7)
+    
+    if (gota == false) {
+        gota = true
+        //  Ocultem tots els leds si volem activar el mode de gota
+        for (let i = 0; i < 5; i++) {
+            for (let j = 0; j < 5; j++) {
+                led.unplot(i, j)
+            }
+        }
+    } else {
+        //  Si l'usuari torna a prèmer el botó B, es torna a posar el mode de estació meteorologica  (REPTE 6)
+        gota = false
+    }
+    
+})
